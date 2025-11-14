9f38a4126ea3a1d1c885470905274a3f6a369a53
Meklati Mohamed <58303418+ivarick@users.noreply.github.com>
2025-11-14 16:21:44 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 from data import vocab_to_int, encode, preprocess
 from model import TransformerSentiment
 
+
 vocab_size = len(vocab_to_int)
 model = TransformerSentiment(vocab_size, max_len=512)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.load_state_dict(torch.load('model.pth', map_location=device))
 model.to(device)
 
 
@@ -13,7 +15,7 @@ def predict_sentiment(text):
     model.eval()
     processed = preprocess(text)
     encoded = encode(processed)
-    input_tensor = torch.tensor([encoded], dtype=torch.long).to(device)
+    input_tensor = torch.tensor([encoded], dtype=torch.long).to(device)  # Move to device
     src_mask = (input_tensor == 0)
     with torch.no_grad():
         output = model(input_tensor, src_mask).squeeze().item()
@@ -24,5 +26,4 @@ print(predict_sentiment("best movie ever"))  # Expected: Positive
 print(predict_sentiment("besides being boring, the scenes were oppressive and dark."))   # Expected: Negative
 print(predict_sentiment("not a really good acting"))   # Expected: Negative
 print(predict_sentiment("2/10"))   # Expected: Negative
-
 print(predict_sentiment("9/10"))  # Expected: Positive
